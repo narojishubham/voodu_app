b3fe6ea764cbb818cfe4ce6787ed43e177725321
narojishubham <shubham.naroji@gmail.com>
2022-06-03 12:22:38 +0530

    auth page layout

--- src/Components/Layouts/AuthLayout.tsx
+++ src/Components/Layouts/AuthLayout.tsx
@@ -1,7 +1,18 @@
+import { Row } from "antd";
+
 const AuthLayout: React.FC<{
     children: React.ReactNode;
 }> = ({ children }) => {
-    return <div>{children}</div>;
+    return (
+        <Row
+            style={{ height: "100vh", backgroundColor: "#f5f5f5" }}
+            className="test213"
+            align="middle"
+            justify="center"
+        >
+            {children}
+        </Row>
+    );
 };
 
 export default AuthLayout;
