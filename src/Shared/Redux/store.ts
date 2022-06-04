9272a8d714b1bd2952f5bbbb5ee241be600f5d4a
narojishubham <shubham.naroji@gmail.com>
2022-06-04 09:54:48 +0530

    added store and getcategory slice

--- src/Shared/Redux/store.ts
+++ src/Shared/Redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import authSlice from "./Reducers/auth.reducer";
+import getCategorySlice from "./Reducers/getCategory.reducer";
+import profileSlice from "./Reducers/profile.reducer";
 
 const store = configureStore({
     reducer: {
         auth: authSlice.reducer,
+        profile:profileSlice.reducer,
+        getCategory:getCategorySlice.reducer,
     },
 });
 
