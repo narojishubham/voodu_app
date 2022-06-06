32bbbe06c8b98efa6e6ea5d9350be3b90f7cd87c
narojishubham <shubham.naroji@gmail.com>
2022-06-06 15:00:25 +0530

    created new api call login with token

---
+++ src/Shared/Redux/Actions/auth/loginWithToken.action.ts
@@ -0,0 +1,29 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import server from "../../../../api";
+import { axiosResHandle, axiosErrHandle } from "../../../../api/axiosHandle";
+import { LoginResTypes } from "../../../Models/Auth/login.types";
+
+export interface LoginParamsType {
+    email: string;
+    password: string;
+}
+
+const loginWithTokenService = () => {
+return server.get<LoginResTypes>("/account/user_profile", {});
+};
+const loginWithTokenAction = createAsyncThunk("auth/loginWithToken", async (_, { rejectWithValue }) => {
+    try {
+        const res = await loginWithTokenService();
+        return axiosResHandle(res);
+    } catch (err) {
+        return rejectWithValue(axiosErrHandle(err));
+    }
+    //   try {
+    //     const res = await loginService(params);
+    //         return(console.log('axiosResHandle: ',axiosResHandle(res)))
+    // } catch (err) {
+    //     console.log('axiosErrHandle: ',axiosErrHandle(err))
+    //     return rejectWithValue(axiosErrHandle(err))
+    // }
+});
+export default loginWithTokenAction;
