26f1cd143adc4d0a94167a47e894b9092c0dff36
narojishubham <shubham.naroji@gmail.com>
2022-06-06 14:37:24 +0530

    added condition to check if token is there if it is there then make api call to login with token

--- src/Components/Routes/AppPagesRoutes.tsx
+++ src/Components/Routes/AppPagesRoutes.tsx
@@ -1,21 +1,28 @@
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { RouterPaths } from "../../api/RouterPaths";
 import { useSelector } from "react-redux";
-import { RootState } from "../../Shared/Redux/store";
 import AppLayout from "../Layouts/AppLayout";
 import { useEffect } from "react";
+import loginWithTokenAction from "../../Shared/Redux/Actions/auth/loginWithToken.action";
+import { RootState, useAppDispatch } from "../../Shared/Redux/store";
 
 const AppPagesRoutes = () => {
+    const dispatch = useAppDispatch();
     let userData = useSelector((state: RootState) => state.auth.userData);
 
     const location = useLocation();
     const navigate = useNavigate();
+    const token = localStorage.getItem("theboom_token");
 
     useEffect(() => {
         console.log({ loggedIn: userData });
         if (!userData) {
-            console.log("logged is null");
-            navigate(RouterPaths.login, { replace: true, state: { from: location } });
+            if (token) {
+                dispatch(loginWithTokenAction()).unwrap();
+            } else {
+                console.log("logged is null");
+                navigate(RouterPaths.login, { replace: true, state: { from: location } });
+            }
         }
     }, [userData]);
 
