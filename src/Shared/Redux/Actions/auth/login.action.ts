3c31aa614afc0af64433c0933d8dcb01f9d7371f
narojishubham <shubham.naroji@gmail.com>
2022-06-07 14:31:34 +0530

    removed interfave and moved  different  folder

--- src/Shared/Redux/Actions/auth/login.action.ts
+++ src/Shared/Redux/Actions/auth/login.action.ts
@@ -1,43 +1,13 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import server from "../../../../api";
 import { axiosResHandle, axiosErrHandle } from "../../../../api/axiosHandle";
+import { LoginResTypes } from "../../../Models/Auth/login.types";
 
 export interface LoginParamsType {
     email: string;
     password: string;
 }
-export type LoginResTypes = {
-    data: {
-        id: number;
-        accountId: number;
-        firstName: string;
-        lastName: string;
-        email: string;
-        phone: string;
-        state: string;
-        createdAt: string;
-        updatedAt: string;
-        account: {
-            id: number;
-            name: string;
-            website: string;
-            contactPerson: string;
-            contactEmail: string;
-            contactPhone: string;
-            state: string;
-            createdAt: string;
-            updatedAt: string;
-            brandCategory: any;
-            designation: any;
-            categoryId: number;
-            designationId: number;
-            posterId: number;
-            qrCode: string;
-            description: string;
-        };
-    };
-    token: string;
-};
+
 const loginService = (data: LoginParamsType) => {
     return (server.post<LoginResTypes>("/session/login", data))
 };
@@ -48,12 +18,5 @@ const loginAction = createAsyncThunk("auth/login", async (params: LoginParamsTyp
     } catch (err) {
         return rejectWithValue(axiosErrHandle(err));
     }
-    //   try {
-    //     const res = await loginService(params);
-    //         return(console.log('axiosResHandle: ',axiosResHandle(res)))
-    // } catch (err) {
-    //     console.log('axiosErrHandle: ',axiosErrHandle(err))
-    //     return rejectWithValue(axiosErrHandle(err))
-    // }
 });
 export default loginAction;
