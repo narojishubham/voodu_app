cf78114ca02551fc5d4f9bb17e3772d5d041211d
narojishubham <shubham.naroji@gmail.com>
2022-06-09 17:59:48 +0530

    update profile action

--- src/Shared/Redux/Actions/profile/updateProfile.action.ts
+++ src/Shared/Redux/Actions/profile/updateProfile.action.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import server from "../../../../api";
-import { axiosErrHandle } from "../../../../api/axiosHandle";
+import { axiosErrHandle, axiosResHandle } from "../../../../api/axiosHandle";
 
 type UpdateProfileData = {
     posterId: string;
@@ -10,14 +10,15 @@ type UpdateProfileData = {
     designationId: number;
 };
 export const uploadProfileDataService = (data: UpdateProfileData) => {
-    return server.patch("/account");
+    return server.patch("/account",data);
 };
 const uploadProfileDataAction = createAsyncThunk(
     "profile/user/upload",
     async (props: UpdateProfileData, { rejectWithValue }) => {
         try {
-            console.log("getPlaylistAction, ", { props });
-            return await uploadProfileDataService(props);
+            // console.log("uploadProfileDataAction, ", { props });
+             const res = await uploadProfileDataService(props);
+             return axiosResHandle(res)
         } catch (err) {
             return rejectWithValue(axiosErrHandle(err));
         }
