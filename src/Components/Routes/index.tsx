b45d3030867cb24dbfeac1074433b6d699803d4d
narojishubham <shubham.naroji@gmail.com>
2022-06-13 12:13:01 +0530

    uncommented new paths

--- src/Components/Routes/index.tsx
+++ src/Components/Routes/index.tsx
@@ -5,6 +5,13 @@ import AuthPagesRoutes from "./AuthPagesRoutes";
 import LoginPage from "../../Pages/Login";
 import SignUpPage from "../../Pages/Signup";
 import OverviewPage from "../../Pages/Overview";
+import ForgotPasswordPage from "../../Pages/ForgotPassword";
+import NewPasswordPage from "../../Pages/NewPassword";
+import ProfilePage from "../../Pages/ProfilePage";
+import Playlist from "../../Pages/Playlists/Playlist";
+import PlaylistDetails from "../../Pages/Playlists/PlaylistDetails";
+import CreatePlaylistPage from "../../Pages/Playlists/CreatePlaylist/CreatePlaylistPage";
+import VideoLibrary from "../../Pages/VideoLibrary/VideoLibrary";
 
 const AppRoutes = () => {
     return (
@@ -13,17 +20,17 @@ const AppRoutes = () => {
                 <Route path={RouterPaths.root}>
                     <Route element={<AppPagesRoutes />}>
                         <Route index element={<Navigate to={RouterPaths.videoLibrary} replace />} />
-                        {/* <Route path={RouterPaths.profile} element={<ProfilePage />} />*/}
+                        <Route path={RouterPaths.profile} element={<ProfilePage />} />
                         <Route path={RouterPaths.overview} element={<OverviewPage />} />
                         <Route path={RouterPaths.videoLibrary}>
-                            {/* <Route index element={<VideoLibrary />} /> */}
+                            <Route index element={<VideoLibrary />} />
                             {/* <Route path={RouterPaths.addPage} element={<UploadVideo />} />
                             <Route path={RouterPaths.p_videoId}>
                                 <Route index element={<VideoDetailsViewEdit />} />
                                 <Route path={RouterPaths.editDetails} element={<VideoDetailsViewEdit />} />
                             </Route> */}
                         </Route>
-                        {/* <Route path={RouterPaths.uploadVideo} element={<UploadVideo />} />
+                        {/* <Route path={RouterPaths.uploadVideo} element={<UploadVideo />} /> */}
 
                         <Route path={RouterPaths.playlists}>
                             <Route index element={<Playlist />} />
@@ -33,14 +40,14 @@ const AppRoutes = () => {
                                 <Route path={RouterPaths.editPage} element={<CreatePlaylistPage />} />
                             </Route>
                         </Route>
-                        <Route path={RouterPaths.settings} element={<div>settings</div>} /> */}
+                        <Route path={RouterPaths.settings} element={<div>settings</div>} />
                     </Route>
 
                     <Route element={<AuthPagesRoutes />}>
                         <Route path={RouterPaths.login} element={<LoginPage />} />
                         <Route path={RouterPaths.signup} element={<SignUpPage />} />
-                        {/*  <Route path={RouterPaths.forgotPassword} element={<ForgotPasswordPage />} />
-                        <Route path={RouterPaths.newPassword} element={<NewPasswordPage />} /> */}
+                        <Route path={RouterPaths.forgotPassword} element={<ForgotPasswordPage />} />
+                        <Route path={RouterPaths.newPassword} element={<NewPasswordPage />} />
                     </Route>
                 </Route>
             </Routes>
