0a8ce5589e2c28c0f671d2bd02868ee077a195a1
narojishubham <shubham.naroji@gmail.com>
2022-06-13 12:22:13 +0530

    custom hook

---
+++ src/Shared/hooks/useCallbackPrompt.ts
@@ -0,0 +1,58 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useCallback, useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useBlocker } from './useBlocker';
+
+export function useCallbackPrompt(when: boolean): (boolean | (() => void))[] {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [showPrompt, setShowPrompt] = useState(/** @type {boolean} */ false);
+  const [lastLocation, setLastLocation] = useState<any>(null);
+  const [confirmedNavigation, setConfirmedNavigation] = useState(false);
+
+  const cancelNavigation = useCallback(() => {
+    setShowPrompt(false);
+  }, []);
+
+  /**
+ * Handle blocking when user click on another route prompt will be shown
+  /**
+ * @callback handleBlockedNavigation 
+ * @return {boolean} - returns true when navigation is confirmed & old location and new location are not same else false
+ */
+  const handleBlockedNavigation = useCallback(
+    (nextLocation) => {
+      // in if condition we are checking next location and current location are equals or not
+      if (
+        !confirmedNavigation &&
+        nextLocation.location.pathname !== location.pathname
+      ) {
+        setShowPrompt(true);
+        setLastLocation(nextLocation);
+        return false;
+      }
+      return true;
+    },
+    [confirmedNavigation]
+  );
+
+  /**
+ * Allows navigation and disables prompt
+  /**
+ * @callback confirmNavigation  
+ */
+  const confirmNavigation = useCallback(() => {
+    setShowPrompt(false);
+    setConfirmedNavigation(true);
+  }, []);
+
+  useEffect(() => {
+    if (confirmedNavigation && lastLocation) {
+      navigate(lastLocation.location.pathname);
+    }
+  }, [confirmedNavigation, lastLocation]);
+
+  useBlocker(handleBlockedNavigation, when);
+
+  return [showPrompt, confirmNavigation, cancelNavigation];
+}
