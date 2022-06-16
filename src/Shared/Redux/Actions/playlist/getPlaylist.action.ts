0d0d3bb05743e35ef87162786642ea34e1742117
narojishubham <shubham.naroji@gmail.com>
2022-06-16 17:37:35 +0530

    added type to getplaylist api

--- src/Shared/Redux/Actions/playlist/getPlaylist.action.ts
+++ src/Shared/Redux/Actions/playlist/getPlaylist.action.ts
@@ -1,18 +1,18 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import server from "../../../../api";
 import { axiosErrHandle, axiosResHandle } from "../../../../api/axiosHandle";
-import { getPlaylist } from "../../../Models/Playlist/playlist.type";
+import { getPlaylist, PlaylistItemType } from "../../../Models/Playlist/playlist.type";
 
 const getPlaylistService =async ({playlistId}:getPlaylist) => {
   const res = await server
-  .get(`/playlists/${playlistId}`, {
+  .get<PlaylistItemType>(`/playlists/${playlistId}`, {
   })
-  console.log(" getPlaylistService, ", {res});
+  // console.log(" getPlaylistService, ", {res});
   return axiosResHandle(res)
 }
 const getPlaylistAction = createAsyncThunk('playlists/item', async (props: getPlaylist, thunkAPI) => {
   try {
-    console.log("getPlaylistAction, ", { props});
+    // console.log("getPlaylistAction, ", { props});
     return await getPlaylistService({playlistId:props.playlistId});
   } catch (err) {
     return thunkAPI.rejectWithValue(axiosErrHandle(err));
