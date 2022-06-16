d248a8908eb10644b639cfc9c17e7c3ea1719194
narojishubham <shubham.naroji@gmail.com>
2022-06-16 17:35:13 +0530

    added types

--- src/Shared/Redux/Actions/feed/uploadVideo/getVideoThumbnail.action.ts
+++ src/Shared/Redux/Actions/feed/uploadVideo/getVideoThumbnail.action.ts
@@ -2,34 +2,31 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import server from "../../../../../api";
 import { axiosErrHandle, axiosResHandle } from "../../../../../api/axiosHandle";
 
-interface GetVideoThumbnailResponse {
-  data?: {
-    accountId?: number;
-    userId?: number;
-    filename?: string;
-    key?: string;
-    valid?: boolean;
-    id?: number;
-    urls?: {
-      original?: string;
+export type GetVideoThumbnailResponse = {
+ accountId: number
+  userId: number
+  filename: string
+  key: string
+  valid: boolean
+  id: number
+  urls:{
+      original: string;
     };
-  };
 }
 type getVideoThumbnail ={
   file: string
 }
 const getVideoThumbnailService = ({file}:getVideoThumbnail) => {
   console.log({ file });
-  return server.post<GetVideoThumbnailResponse>('/resources/snapShot', { file })
+  return server.post('/resources/snapShot', { file })
 };
 export const getVideoThumbnailAction = createAsyncThunk(
     'video/uploadUsingReqId',
     async (props: getVideoThumbnail,  thunkAPI) => {
         const { file } = props;
         try {
-            const response = await getVideoThumbnailService({  file });
+            const response = await getVideoThumbnailService({ file });
              console.log('getVideoThumbnailAction',response)
-
             return axiosResHandle(response)
         } catch (error: any) {
             return axiosErrHandle(error);
