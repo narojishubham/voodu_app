2ccbfd65f8811aad6bc3c49c957738a31b396823
narojishubham <shubham.naroji@gmail.com>
2022-06-16 17:36:56 +0530

    delete playlist api

--- src/Shared/Redux/Actions/playlist/deletePlaylistItem.action.ts
+++ src/Shared/Redux/Actions/playlist/deletePlaylistItem.action.ts
@@ -8,9 +8,9 @@ type deletePlaylistItemTypes ={
 }
 
 const deletePlaylistItemService =async ({_id}:deletePlaylistItemTypes) => {
-   return( server.delete<PlaylistItemDeleteResType>(`/playlists/${_id}`))
+   return( server.delete(`/playlists/${_id}`))
 }
-const deletePlaylistItemAction = createAsyncThunk('playlists/item', async (props: deletePlaylistItemTypes, thunkAPI) => {
+const deletePlaylistItemAction = createAsyncThunk('delete/playlists/', async (props: deletePlaylistItemTypes, thunkAPI) => {
     try {
       return await deletePlaylistItemService(props);
     } catch (err) {
